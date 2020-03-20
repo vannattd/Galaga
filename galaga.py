911c37a388cff00c38bcf61d3bfd34c39a4456a1
Dylan Vannatter <54507891+vannattd@users.noreply.github.com>
2020-03-20 16:41:14 -0400

    comments

--- galaga.py
+++ galaga.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+# Group members: Logan Jaglowski and Dylan Vannatter
+# version 1.0 3-20-2020
+# CIS 343 01
+# Prof. Woodring
+# Files: galaga.py
+# Purpose: Creating Galaga using Python 3 w/ pygame
+# Game: Win by destroying all the enemy ships. Lose if you run out of lives. Shoot power up to increase speed.
+# Controls: Move with left and right arrow keys and shoot with space bar or 'a'.
 
 import pygame
 import random
@@ -14,6 +22,7 @@ class Overlay(pygame.sprite.Sprite):
         self.image.fill((0, 0, 0))
         self.rect = self.image.get_rect()
         self.font = pygame.font.Font('freesansbold.ttf', 18)
+        # Overlay to keep track of our lives and score
         self.render('Score: 0        Lives: 5')
 
     def render(self, text):
@@ -30,6 +39,7 @@ class Overlay(pygame.sprite.Sprite):
 class Speed(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        # Loading image in for speed power up
         self.image = pygame.image.load('assets/speed.png')
         self.rect = self.image.get_rect()
         self.rect.x = 0
@@ -39,6 +49,7 @@ class Speed(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        # Loading in player image and position
         self.image = pygame.image.load('assets/player.png')
         self.rect = self.image.get_rect()
         self.rect.x = 300
@@ -51,12 +62,14 @@ class Player(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        # Loading in enemy image
         self.image = pygame.image.load('assets/enemy.png')
         self.rect = self.image.get_rect()
         self.vector = [5, 0]
         self.iterator = 0
 
     def update(self, enemies):
+        # Making enemies move and stay within boundaries
         for enemy in enemies:
             if enemy.rect.x >= 745:
                 self.vector[0] = -5
@@ -77,6 +90,7 @@ class Enemy(pygame.sprite.Sprite):
 class Laser(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        # Loading in laser image
         self.image = pygame.image.load('assets/laser.png')
         self.rect = self.image.get_rect()
         self.rect.x = 500
@@ -88,17 +102,20 @@ class Laser(pygame.sprite.Sprite):
         if self.rect.y > 600 | self.rect.y < 0:
             self.kill()
         hitObject = pygame.sprite.spritecollideany(self, enemies)
+        # How to react when laser hits an enemy
         if hitObject:
             if self.vector[1] < 0:
                 self.thud_sound.play()
                 hitObject.kill()
                 self.kill()
                 game.score += 1
+        # How to react if laser hits player
         if pygame.sprite.collide_rect(self, player):
             if self.vector[1] > 0:
                 self.kill()
                 game.lives -= 1
         hitPowerUp = pygame.sprite.spritecollideany(self, totalpowerup)
+        # How to react when laser hits power up
         if hitPowerUp:
             if self.vector[1] < 0:
                 game.powerUp = 1
@@ -131,6 +148,7 @@ class Game:
         self.powerUp = 0
         self.existingPower = 0
         self.powerUpCounter = 0
+        # Spawning enemies
         for i in range(0, 4):
             for j in range(0, 6):
                 enemy = Enemy()
@@ -142,6 +160,7 @@ class Game:
         self.done = False
         while not self.done:
             self.screen.fill((255, 205, 255))
+            # How to react when lives are gone or enemies are gone
             for event in pygame.event.get():
                 if self.lives <= 0 or self.score == 24:
                     pygame.quit()
@@ -149,6 +168,7 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.done = True
                 if event.type == pygame.KEYDOWN:
+                    # Key press responses
                     if event.key == pygame.K_a:
                         laser = Laser()
                         laser.rect.x = self.player.rect.x + 43
@@ -175,8 +195,7 @@ class Game:
                             self.player.rect.x += 10
                         if self.player.rect.x >= 750:
                             self.player.rect.x = 750
-                # if self.ready:
-                # self.balls.sprites()[0].rect.x = self.paddle.rect.x + 25
+            # Enemies shooting lasers
             for enemy in self.enemies:
                 if random.randint(0, 1000) == 1:
                     laser = Laser()
@@ -185,7 +204,7 @@ class Game:
                     laser.rect.y = enemy.rect.y + 10
                     laser.vector = [0, 2]
                     self.projectiles.add(laser)
-
+            # Randomly spawning power up
             if random.randint(0, 100) == 1:
                 if self.powerUp == 0:
                     if self.existingPower == 0:
@@ -195,6 +214,7 @@ class Game:
                         self.existingPower = 1
                         self.totalPowerUps.add(speed)
 
+            # Keeping track of power ups
             if self.powerUpCounter == 400:
                 self.powerUp = 0
                 self.powerUpCounter = 0
@@ -202,7 +222,9 @@ class Game:
             if self.powerUp == 1:
                 self.powerUpCounter += 1
 
-            self.projectiles.update(self, self.enemies, self.player, self.totalPowerUps, self.powerUp, self.existingPower)
+            # Updating the screen
+            self.projectiles.update(self, self.enemies, self.player, self.totalPowerUps, self.powerUp,
+                                    self.existingPower)
             self.overlay.update(self.score, self.lives)
             self.enemies.update(self.enemies)
             self.projectiles.draw(self.screen)
@@ -212,20 +234,6 @@ class Game:
             self.totalPowerUps.draw(self.screen)
             pygame.display.flip()
             self.clock.tick(60)
-
-
-class Intro(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((800, 120))
-        self.font = pygame.font.Font('freesansbold.ttf', 96)
-        self.text = self.font.render('Breakout!', True, (0, 0, 0))
-        self.rect = self.image.get_rect()
-        self.image.blit(self.text, self.rect)
-
-    def draw(self, screen):
-        screen.blit(self.text, (0, 0))
-
 
 if __name__ == "__main__":
     game = Game()
